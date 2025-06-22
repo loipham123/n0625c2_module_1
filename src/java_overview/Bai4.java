19bfa36a409e608e05025f6389b55d130ac1913d
loipham123 <phuocloi100204@gmail.com>
2025-06-22 15:02:49 +0700

    fix comment

--- src/java_overview/Bai4.java
+++ src/java_overview/Bai4.java
@@ -12,19 +12,19 @@ public class Bai4 {
         System.out.print("Nhap vao diem Toan: ");
         double diemToan = sc.nextDouble();
         System.out.print("Nhap he so  Toan: ");
-        double hsToan= sc.nextDouble();
+        double hsToan = sc.nextDouble();
 
         System.out.print("Nhap vao diem Ly: ");
         double diemLy = sc.nextDouble();
         System.out.print("Nhap he so  Ly: ");
-        double hsLy= sc.nextDouble();
+        double hsLy = sc.nextDouble();
 
         System.out.print("Nhap vao diem Hoa: ");
         double diemHoa = sc.nextDouble();
         System.out.print("Nhap he so  Hoa: ");
-        double hsHoa= sc.nextDouble();
+        double hsHoa = sc.nextDouble();
 
-        double diemTB=(diemToan*hsToan+diemLy*hsLy+diemHoa*hsHoa)/(hsToan+hsLy+hsHoa);
+        double diemTB = (diemToan * hsToan + diemLy * hsLy + diemHoa * hsHoa) / (hsToan + hsLy + hsHoa);
         System.out.print("Diem trung binh cua ban la: " + diemTB);
     }
 }
